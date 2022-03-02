394027d5ed14d3e045183a936984ecb71300fcc5
Alequita <alejandroalonso0306@gmail.com>
2022-03-02 17:18:06 -0300

    Changed main view

--- amano/urls.py
+++ amano/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
     path('purchases/', include('purchases.urls', namespace='purchases')),
-    path('stock/', include('stock.urls', namespace='stock')),
+    path('', include('stock.urls', namespace='stock')),
     path('sales/', include('sales.urls', namespace='sales')),
     path('recap/', include('recap.urls', namespace='recap')),
 ]
