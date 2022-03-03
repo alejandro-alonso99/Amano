41a57e4525b445e9533dac8f18e6f42fc7d8cd3a
Alequita <alejandroalonso0306@gmail.com>
2022-03-03 12:25:41 -0300

    remove postgres uri

--- amano/settings.py
+++ amano/settings.py
@@ -94,9 +94,10 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=600)
+dj_database_url.config(conn_max_age=600)
 
-DATABASES['default'] = dj_database_url.config(default='postgres://zipucfhkmujxuy:ed85bffef8c45a1c5c888ba1757dd15430654dfa0c21159f2496c277dc52c702@ec2-34-200-161-87.compute-1.amazonaws.com:5432/d7sas66vsqfred')
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
